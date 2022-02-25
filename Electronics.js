27e1fc8f7438f24797fd583c1453c3882411d781
dom111 <dom111@users.noreply.github.com>
2022-02-25 20:36:23 +0000

    Initial commit.

---
+++ Electronics.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Electronics = void 0;
+const Advance_1 = require("@civ-clone/core-science/Advance");
+class Electronics extends Advance_1.default {
+}
+exports.Electronics = Electronics;
+exports.default = Electronics;
+//# sourceMappingURL=Electronics.js.map
